d4dbec1313f73247c5db647a988b577b521071fc
Rokib Shuvo <itismehey975@gmail.com>
2025-06-25 11:47:56 +0600

    docor and pharmasicst are separated

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
+//healthcare project server side.
 require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 7000;
 
 // middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 //cookieoptions
 const cookieOptions = {
-  httpOnly:true,
-  secure:process.env.NODE_ENV === "production",
-  sameSite:process.env.NODE_ENV === "production" ? "none" : "strict",
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
 };
 
 //Mongodb configuration
@@ -30,7 +37,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     console.log("Connected to MongoDB successfully!");
     //user collection
     const db = client.db("AlshefaAdmin");
@@ -43,22 +50,29 @@ async function run() {
     const orderRecords = client.db("AlshefaAdmin").collection("medicineOrder");
     //medicine storage
     const medicineStorage = client.db("AlshefaAdmin").collection("medicine");
-
+    // doctor and
+    const doctorsCollection = client.db("AlshefaAdmin").collection("doctors");
+    //pharmasict list
+    const pharmasictCollection = client
+      .db("AlshefaAdmin")
+      .collection("pharmasicts");
     // generate jwt token
-    app.post("/jwt",async(req,res) => {
+    app.post("/jwt", async (req, res) => {
       const user = req.body;
       //validation for user
       if (!user || !user) {
-         return res.status(400).json({ error: "Invalid user data" });
+        return res.status(400).json({ error: "Invalid user data" });
       }
-      const jwttoken = jwt.sign(user,process.env.Access_Token_Secret,{expiresIn:"2h"})
+      const jwttoken = jwt.sign(user, process.env.Access_Token_Secret, {
+        expiresIn: "2h",
+      });
 
-      res.cookie("token",jwttoken,cookieOptions).send({success:true});
-    })
-   //clear jwt token while logout
-   app.post("/jwt-logout",async(req,res) => {
-    res.clearCookie("token",cookieOptions).send({success:true})
-   });
+      res.cookie("token", jwttoken, cookieOptions).send({ success: true });
+    });
+    //clear jwt token while logout
+    app.post("/jwt-logout", async (req, res) => {
+      res.clearCookie("token", cookieOptions).send({ success: true });
+    });
     //  user creation operation
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -85,7 +99,7 @@ async function run() {
     // update user status by id params
     app.patch("/update-role/:id", async (req, res) => {
       const id = req.params.id;
-      const role = req.body.role;
+      const role = req.body.newRole;
       const filter = { _id: new ObjectId(id) };
       const updateStatus = {
         $set: {
@@ -96,7 +110,7 @@ async function run() {
       res.send(result);
     });
     //appoinment booking by patient
-    app.post("/appoinment", async (req, res) => {
+    app.post("/appoinments", async (req, res) => {
       const appoint = req.body;
       const result = await appointmentCollection.insertOne(appoint);
       res.send(result);
@@ -105,7 +119,23 @@ async function run() {
     app.get("/appoinments", async (req, res) => {
       const email = req.query.email;
       if (email) {
-        const query = { email: email };
+        const query = {
+          $or: [{ doctoremail: email }, { patientemail: email }],
+        };
+        // console.log(query);
+
+        const queryResult = await appointmentCollection.find(query).toArray();
+        res.send(queryResult);
+      } else {
+        const result = await appointmentCollection.find().toArray();
+        res.send(result);
+      }
+    });
+    // unconfirmed appoinments
+    app.get("/appoinments/unconfirmed", async (req, res) => {
+      const email = req.query.email;
+      if (email) {
+        const query = { docemail: email, status: { $ne: "Confirmed" } };
         const queryResult = await appointmentCollection.find(query).toArray();
         res.send(queryResult);
       } else {
@@ -114,9 +144,9 @@ async function run() {
       }
     });
     // status update of appoinments
-    app.patch("/appoinments/:id", async (req, res) => {
+    app.patch("/update-appoinment/:id", async (req, res) => {
       const id = req.params.id;
-      const status = req.body.status;
+      const status = req.body.newStatus;
       const query = { _id: new ObjectId(id) };
       const updateStatus = {
         $set: {
@@ -134,9 +164,11 @@ async function run() {
     });
     //get all order and also by query
     app.get("/orderMedi", async (req, res) => {
-      const email = req.query.email;
-      if (email) {
-        const query = { email: email };
+      const clientSidemail = req.query.email;
+      if (clientSidemail) {
+        const query = clientSidemail ? { customeremail: clientSidemail } : {};
+        // console.log(email,query);
+
         const queryResult = await orderRecords.find(query).toArray();
         res.send(queryResult);
       } else {
@@ -147,7 +179,9 @@ async function run() {
     //update medicine order status
     app.patch("/orderMedi/:id", async (req, res) => {
       const id = req.params.id;
-      const status = req.body.status;
+      const status = req.body.newStatus;
+      // console.log(id,status);
+
       const query = { _id: new ObjectId(id) };
       const updateStatus = {
         $set: {
@@ -181,6 +215,39 @@ async function run() {
       const result = await medicineStorage.updateOne(query, updateStatus);
       res.send(result);
     });
+    //create doctor
+    app.post("/doctors", async (req, res) => {
+      const doctorData = req.body;
+
+      const result = await doctorsCollection.insertOne(doctorData);
+      res.send(result);
+    });
+    //create pharmasists
+    app.post("/pharmasicts", async (req, res) => {
+      const pharmaData = req.body;
+
+      const result = await pharmasictCollection.insertOne(pharmaData);
+      res.send(result);
+    });
+    //get doctors
+    app.get("/doctors", async (req, res) => {
+      const id = req.query._id;
+      if (id) {
+        const query = { _id: new ObjectId(id) };
+        const queryResult = await doctorsCollection.findOne(query);
+        res.send(queryResult);
+      } else {
+        const result = await doctorsCollection.find().toArray();
+        res.send(result);
+      }
+    });
+    //get Pharmasists
+    app.get("/pharmacists", async (req, res) => {
+      const result = await pharmasictCollection.find().toArray();
+      res.send(result);
+    });
+    //get doctor by id
+    
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
